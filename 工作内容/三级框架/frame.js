035e56a9e1988624c94fa47e6e5057fa4f5aae47
HARRISKING <llc13588440410@outlook.com>
2017-03-24 20:27:37 +0800

    3.27

--- 工作内容/三级框架/frame.js
+++ 工作内容/三级框架/frame.js
@@ -1,15 +1,72 @@
+
+
+
 $('.btn0').on('click',function(){
-  $('.panel-all').append('<div class="panel"><div class="box box1">大框<div class="btn1 btn">+</div></div><div class="box box2">中框<div class="btn2 btn">+</div></div><div class="box box3">小框<div class="btn3 btn">+</div></div></div>');
+  $('.panel-all').append('<p><input class="inp" /><button class="addone">+</button></p> ');
+});
+$( '.panel-all' ).on( 'click', "button.addone", function () {
+   $(this).parent("p").append('<p style="margin-left:20px;"><input class="inp" /><button class="addone">+</button></p> ');
+});
+
+
+
+function getP(father){
+  var len = $(father).children('p').length;
+  console.log(len);
+
+  //判断子元素是否存在
+  if(len !== 0){
+
+    var child = [];
+
+    for(var i=0; i<len; i++){
+        //定义空对象
+        var obj ={};
+        //空对象中添加属性name
+        obj.name = $(father).children('p').children('input').eq(i).val();
+        console.log(obj);
+        //将对象推送到上层的obj.child中
+        child.push(obj);
+        //定义这一层的OBJ.child
+        obj.child = [];
+        // console.log(obj.child);
+        // obj.child.push(child);
+    };
+    console.log(child);
+    // for(var keys in child){
+    //   var json = {
+    //     data:child[keys]
+    //   }
+    //   obj.child.push(json);
+    // }
+    getP($(father).children('p'));
+  }
+};
+$('button').on('click',function(){
+  getP('.panel-all');
 });
 
 
 
 
 
-$('.btn1').on('click',function(){
-  $(this).parents().parents().find('.box2').show();
-});
+//备份区：
 
-$('.btn2').on('click',function(){
-  $(this).parents().find('.box3').show();
-})
+// var json = [];
+// function getP(father){
+//   var len = $(father).children('p').length;
+//   console.log(len);
+//
+//   for(var i=0; i<len; i++){
+//       var obj ={};
+//       obj.name = $(father).children('p').children('input').eq(i).val();
+//       obj.child = [];
+//       console.log(obj);
+//       json.push(obj);
+//       console.log(json);
+//   };
+// };
+//
+// $('button').on('click',function(){
+// getP('.panel-all');
+// });
